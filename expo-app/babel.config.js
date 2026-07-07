54c69584a2ad320686a6457cb8019daf61f8713e
Jay Rane <127013993+Jay-shm@users.noreply.github.com>
2026-07-08 03:08:21 +0530

    fix: apply hermes babel fixes only to event-target-shim to prevent DOMException crashes

--- expo-app/babel.config.js
+++ expo-app/babel.config.js
@@ -2,11 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      '@babel/plugin-transform-typescript',
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+    overrides: [
+      {
+        test: /node_modules[\\\/]event-target-shim/,
+        plugins: [
+          ['@babel/plugin-transform-class-properties', { loose: true }],
+          ['@babel/plugin-transform-private-methods', { loose: true }]
+        ]
+      }
     ]
   };
 };
